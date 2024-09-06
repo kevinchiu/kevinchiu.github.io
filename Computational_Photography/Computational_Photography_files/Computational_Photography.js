d24d41fdbf14662dc60418ae1f65d2f172aaaba6
Kevin Chiu <kevin.gc@gmail.com>
2024-09-06 00:20:53 -1000

    merge new kevinchiu.org over old

--- Computational_Photography/Computational_Photography_files/Computational_Photography.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20101213
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Computational_Photography_files/Computational_PhotographyMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
